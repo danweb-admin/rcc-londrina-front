c2d1c519d3614bddf61b00562ca72d512a0e33e6
William Nascimento <william.nascimento2010@gmail.com>
2023-12-31 17:57:10 -0300

    alterando rotando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { AfterContentChecked, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,10 +9,11 @@ import { AfterContentChecked, ChangeDetectorRef, Component, OnInit } from '@angu
 })
 export class AppComponent implements OnInit{
 
-    constructor() {
-      }
+    constructor(private router: Router) {
+      
+    }
 
     ngOnInit() {
-        
+      this.router.navigate(['/login']);
     }
 }
